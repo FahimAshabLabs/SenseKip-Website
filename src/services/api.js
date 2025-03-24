e624f31f454dce83a29ce7449138e6ccffb70746
FahimAshabLabs <fahim2ashab@gmail.com>
2025-03-24 15:50:13 +0600

    ip upadated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000'; // Update if needed
+const API_BASE_URL = 'http://3.140.62.93:8000/'; // Update if needed
+
 
 export const loginUser = async (email, password) => {
     const response = await axios.post(`${API_BASE_URL}/accounts/users/login/`, { email, password });
